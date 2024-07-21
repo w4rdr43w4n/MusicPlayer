57801287dee8d402612024de6f36a2b1e978ffe0
w4rdr43w4n <zwerdos123@gmail.com>
2024-07-21 21:39:36 +0300

    Upload Project Files

---
+++ app.js
@@ -0,0 +1,18 @@
+/* This code if you want to complete the project
+ to make it a web application, but currently there 
+ is no need to do so, because it just a Music Player :)
+ */
+
+
+ /*
+var http = require('http');
+var fs = require('fs');
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  
+  fs.createReadStream('main.html').pipe(res);
+
+}).listen(8080);
+*/
